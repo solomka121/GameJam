01a76c443f0e4dcca456ed7d07f95a4c13e6e0bc
NooB <oleg.nakhaevgame@mail.ru>
2021-03-06 19:54:39 +0300

    Add Tramp

---
+++ GameJam/Assets/Scripts/OlegN/RotateTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateTrap : MonoBehaviour
+{
+    [SerializeField] private float _speedRotate;
+    [SerializeField] private float _forceBack;
+    [SerializeField] private float _forceUp;
+    void Update()
+    {
+        transform.Rotate(Vector3.up * _speedRotate * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.gameObject.transform.forward * _forceBack, ForceMode.Impulse);
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.gameObject.transform.forward * _forceUp, ForceMode.Impulse);
+        }
+    }
+}
