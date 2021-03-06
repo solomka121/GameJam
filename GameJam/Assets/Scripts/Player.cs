6697bddc6b8ebe06d2d56da60fecf215ffbb6c14
Slava <slavasolomahin12@gmail.com>
2021-03-06 05:29:37 +0200

    AddedCinemachine

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -13,21 +13,30 @@ public class Player : MonoBehaviour
     [SerializeField] private float _speed;
     [SerializeField] private float _speedInJump;
     [SerializeField] private float _maxVelocityInAir;
-    [SerializeField] private float _airCurrentVelocity;
+    [SerializeField] private float _maxVelocityOnGround;
 
     [Header("HP")]
     [SerializeField] private int _countHp;
-    private int _hp; 
+    private int _hp;
 
     [Header("WallRun")]
     [SerializeField] private LayerMask _whatIsWallRun;
     [SerializeField] private Transform _orientation;
-    [SerializeField] private float wallrunForce, maxWallrunTime, maxWallSpeed;
-    [SerializeField] private float maxWallRunCameraTilt, wallRunCameraTilt;
-    private bool isWallRight, isWallLeft, isWallRunning;
+    [SerializeField] private float _wallrunForce, _maxWallrunTime, _maxWallSpeed;
+    [SerializeField] private float _maxWallRunCameraTilt, _wallRunCameraTilt;
+    [SerializeField] private bool _isWallRight, _isWallLeft, _isWallRunning;
 
     [Header("Camera")]
     [SerializeField] private Transform _camera;
+
+    [Header("Drag controller")]
+    [SerializeField] private float _maxGroundDrag;
+    [SerializeField] private float _minGroundDrag;
+    [SerializeField] private float _normalSpeed;
+    [SerializeField] private float _iceSpeed;
+    [SerializeField] private LayerMask _highFrictionSurface;
+    [SerializeField] private LayerMask _lowFrictionSurface;
+    private float _drag;
 
     private Vector3 _nowCheckPoint;
     private bool _ground;
@@ -36,6 +45,8 @@ public class Player : MonoBehaviour
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
+        _drag = _rb.drag;
+        _rb.drag = _maxGroundDrag;
         _ground = false;
         _hp = _countHp;
     }
@@ -62,6 +73,7 @@ public class Player : MonoBehaviour
         {
             _ground = false;
         }
+
     }
 
     #endregion
@@ -74,35 +86,31 @@ public class Player : MonoBehaviour
 
         Vector3 move = new Vector3(horizontal, 0, vertical);
 
-        if(_ground)
+        if (_ground)
         {
-			if (move.magnitude >= 0.1f)
-			{
+            if (move.magnitude >= 0.1f)
+            {
                 float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
 
                 Vector3 movementDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
                 _rb.AddForce(movementDirection * _speed, ForceMode.Force);
-
-			}
+            }
 
             if (Input.GetButtonDown("Jump"))
             {
                 _rb.AddForce(Vector3.up * _forceJump);
             }
-
-            _airCurrentVelocity = _rb.velocity.magnitude;
         }
         else
         { //add velocity limit
-            float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
-
-            Vector3 movementDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
-
-            if (_airCurrentVelocity > _maxVelocityInAir)
+            if (move.magnitude >= 0.1f)
             {
-                _rb.velocity = _rb.velocity.normalized * _airCurrentVelocity;
+                float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + _camera.eulerAngles.y;
+
+                Vector3 movementDirection = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                _rb.AddForce(movementDirection * _speedInJump, ForceMode.Force);
             }
-            _rb.AddForce(movementDirection * _speedInJump, ForceMode.Force);
+
         }
     }
 
@@ -119,11 +127,11 @@ public class Player : MonoBehaviour
             _rb.AddTorque(Vector3.right * Input.GetAxis("Vertical") * 10);
         }
 
-		if (Input.GetButtonDown("Jump") && _ground)
-		{
-			_rb.AddForce(Vector3.up * _forceJump);
-		}
-	}
+        if (Input.GetButtonDown("Jump") && _ground)
+        {
+            _rb.AddForce(Vector3.up * _forceJump);
+        }
+    }
 
     #endregion
 
@@ -133,11 +141,11 @@ public class Player : MonoBehaviour
     {
         _hp -= damage;
 
-        if(_hp <= 0)
+        if (_hp <= 0)
         {
             Death();
         }
-        else 
+        else
         {
             Respawn();
         }
@@ -159,7 +167,7 @@ public class Player : MonoBehaviour
 
     private void Death()
     {
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 
     public void Respawn()
@@ -184,56 +192,56 @@ public class Player : MonoBehaviour
     private void WallRunInput() //make sure to call in void Update
     {
         //Wallrun
-        if (Input.GetKey(KeyCode.D) && isWallRight) StartWallrun();
-        if (Input.GetKey(KeyCode.A) && isWallLeft) StartWallrun();
+        if (Input.GetKey(KeyCode.D) && _isWallRight) StartWallrun();
+        if (Input.GetKey(KeyCode.A) && _isWallLeft) StartWallrun();
     }
     private void StartWallrun()
     {
         _rb.useGravity = false;
-        isWallRunning = true;
+        _isWallRunning = true;
 
-        if (_rb.velocity.magnitude <= maxWallSpeed)
+        if (_rb.velocity.magnitude <= _maxWallSpeed)
         {
-            _rb.AddForce(_orientation.forward * wallrunForce * Time.deltaTime);
+            _rb.AddForce(_orientation.forward * _wallrunForce * Time.deltaTime);
 
             //Make sure char sticks to wall
-            if (isWallRight)
-                _rb.AddForce(_orientation.right * wallrunForce / 5 * Time.deltaTime);
+            if (_isWallRight)
+                _rb.AddForce(_orientation.right * _wallrunForce / 5 * Time.deltaTime);
             else
-                _rb.AddForce(-_orientation.right * wallrunForce / 5 * Time.deltaTime);
+                _rb.AddForce(-_orientation.right * _wallrunForce / 5 * Time.deltaTime);
         }
     }
     private void StopWallRun()
     {
-        isWallRunning = false;
+        _isWallRunning = false;
         _rb.useGravity = true;
     }
     private void CheckForWall() //make sure to call in void Update
     {
-        isWallRight = Physics.Raycast(transform.position, _orientation.right, 1f, _whatIsWallRun);
-        isWallLeft = Physics.Raycast(transform.position, -_orientation.right, 1f, _whatIsWallRun);
+        _isWallRight = Physics.Raycast(transform.position, _orientation.right, 1f, _whatIsWallRun);
+        _isWallLeft = Physics.Raycast(transform.position, -_orientation.right, 1f, _whatIsWallRun);
 
         //leave wall run
-        if (!isWallLeft && !isWallRight) StopWallRun();
+        if (!_isWallLeft && !_isWallRight) StopWallRun();
     }
 
     private void WallJump()
     {
-        if (isWallRunning)
+        if (_isWallRunning)
         {
 
             //normal jump
-            if (isWallLeft && !Input.GetKey(KeyCode.D) || isWallRight && !Input.GetKey(KeyCode.A))
+            if (_isWallLeft && !Input.GetKey(KeyCode.D) || _isWallRight && !Input.GetKey(KeyCode.A))
             {
                 _rb.AddForce(Vector2.up * _forceJump * 1.5f);
                 _rb.AddForce(Vector3.up * _forceJump * 0.5f);
             }
 
             //sidwards wallhop
-            if (isWallRight || isWallLeft && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) _rb.AddForce(-_orientation.up * _forceJump * 1f);
-            if (isWallRight || isWallLeft && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) _rb.AddForce(-_orientation.up * _forceJump * 1f);
-            if (isWallRight && Input.GetKey(KeyCode.A)) _rb.AddForce(-_orientation.right * _forceJump * 3.2f);
-            if (isWallLeft && Input.GetKey(KeyCode.D)) _rb.AddForce(_orientation.right * _forceJump * 3.2f);
+            if (_isWallRight || _isWallLeft && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) _rb.AddForce(-_orientation.up * _forceJump * 1f);
+            if (_isWallRight || _isWallLeft && Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) _rb.AddForce(-_orientation.up * _forceJump * 1f);
+            if (_isWallRight && Input.GetKey(KeyCode.A)) _rb.AddForce(-_orientation.right * _forceJump * 3.2f);
+            if (_isWallLeft && Input.GetKey(KeyCode.D)) _rb.AddForce(_orientation.right * _forceJump * 3.2f);
 
             //Always add forward force
             _rb.AddForce(_orientation.forward * _forceJump * 1f);
@@ -241,14 +249,113 @@ public class Player : MonoBehaviour
     }
     #endregion
 
+    #region RbDragController
+    private void OnCollisionEnter(Collision collision)
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0, vertical);
+
+        #region HighFriction ground (drag: 0.2 - 2)
+        if (collision.gameObject.layer == 11)//high friction layer
+        {
+            _speed = _normalSpeed;
+            if (move.magnitude > 0.3f)
+            {
+                //if moves
+                if (_rb.drag > _minGroundDrag)
+                {
+                    //smoothly change drag to _minGroundDrag
+                    StartCoroutine(ChangeDrag(_rb.drag, _minGroundDrag, 1f));
+
+                }
+            }
+            if (move.magnitude < 0.3f)
+            {
+                //if stands
+                if (_rb.drag < _maxGroundDrag)
+                {
+                    //smoothly plus drag value to _maxGroundDrag
+                    StartCoroutine(ChangeDrag(_rb.drag, _maxGroundDrag, 1f));
+                }
+            }
+
+        }
+        #endregion
+
+        #region LowFriction ground (drag: 0.2)
+        if (collision.gameObject.layer == 12)
+        {
+            _speed = _iceSpeed;
+            if (_ground)
+            {
+                if (_rb.drag > _minGroundDrag)
+                {
+                    //smoothly change drag to _minGroundDrag
+                    StartCoroutine(ChangeDrag(_rb.drag, _minGroundDrag, 1f));
+
+                }
+            }
+        }
+        #endregion
+    }
+
+    private void RbDragInAir()
+    {
+        if (!_ground)
+        {
+            _speed = _normalSpeed;
+            _rb.drag = _minGroundDrag;
+
+        }
+    }
+    private void VelocityInAir()
+    {
+        if (!_ground)
+        {
+            if (_rb.velocity.magnitude > _maxVelocityInAir)
+            {
+                _rb.AddForce(-_rb.velocity * _speedInJump / 9, ForceMode.Force);
+            }
+            _rb.AddForce(Vector2.down * 7);
+        }
+    }
+    private void VelocityOnGround()
+    {
+        if (_ground)
+        {
+
+            if (_rb.velocity.magnitude > _maxVelocityOnGround)
+            {
+                _rb.AddForce(-_rb.velocity * _speed / 10, ForceMode.Force);
+            }
+        }
+    }
+
+    IEnumerator ChangeDrag(float drag_start, float drag_end, float duration)
+    {
+        float elapsed = 0.0f;
+        while (elapsed < duration)
+        {
+            _rb.drag = Mathf.Lerp(drag_start, drag_end, duration);
+            elapsed += Time.deltaTime;
+            yield return null;
+
+        }
+        _rb.drag = drag_end;
+    }
+    #endregion
+
+
     void Update()
     {
-        if(_force)
+        if (_force)
         {
             Move();
         }
 
-        if(!_force)
+        if (!_force)
         {
             MoveTurque();
         }
@@ -260,5 +367,9 @@ public class Player : MonoBehaviour
 
         WallRunInput();
         CheckForWall();
+        RbDragInAir();
+        VelocityInAir();
+        VelocityOnGround();
+
     }
 }
