499a71169be0894d460d44ee93b4117bc757a75f
NooB <oleg.nakhaevgame@mail.ru>
2021-03-07 00:49:20 +0300

    1-3 lvl

---
+++ GameJam/Assets/Scripts/OlegN/FollingPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollingPlatform : MonoBehaviour
+{
+    [SerializeField] private float _timeCrash;
+    [SerializeField] private float _timeSpawn;
+
+    private Rigidbody _rb;
+    private Vector3 _startTransform;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.isKinematic = true;
+    }
+
+    private void Start()
+    {
+        _startTransform = transform.position;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            Invoke(nameof(CrashMethod), _timeCrash);
+        }
+    }
+
+    private void CrashMethod()
+    {
+        _rb.isKinematic = false;
+        Invoke(nameof(SpawnMethod), _timeSpawn);
+    }
+
+    private void SpawnMethod()
+    {
+        _rb.isKinematic = true;
+        transform.position = _startTransform;
+    }
+}
---
+++ GameJam/Assets/Scripts/OlegN/TerningPlatforms.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerningPlatforms : MonoBehaviour
+{
+    [SerializeField] private float _timeWait;
+    [SerializeField] private float _speedRotate;
+    private float _waitTime;
+    private void Update()
+    {
+        if (transform.rotation.z >= 0.9999f || transform.rotation.z <= -0.9999f)
+        {
+            transform.rotation = new Quaternion(0, 0, 0, 0);
+            _waitTime = _timeWait;
+        }
+        if (_waitTime <= 0f)
+        {
+            transform.Rotate(Vector3.forward * _speedRotate * Time.deltaTime);
+        }
+        _waitTime -= Time.deltaTime;
+    }
+}
