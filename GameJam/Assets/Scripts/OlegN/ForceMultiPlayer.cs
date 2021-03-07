5f872eb52f49ac7925f505821405cf7cb22b46b0
NooB <oleg.nakhaevgame@mail.ru>
2021-03-07 11:59:19 +0300

    SlavaTest

---
+++ GameJam/Assets/Scripts/OlegN/ForceMultiPlayer.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForceMultiPlayer : MonoBehaviour
+{
+    [SerializeField] private float _forceMultiPlayer;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.gameObject.GetComponent<Rigidbody>().velocity *= _forceMultiPlayer; 
+        }
+    }
+}
