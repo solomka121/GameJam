8abdc12fef80d10eeec25196cc709c2c14543809
NooB <oleg.nakhaevgame@mail.ru>
2021-03-07 08:36:42 +0300

    TempSave

--- GameJam/Assets/Scripts/CheckPoint.cs
+++ GameJam/Assets/Scripts/CheckPoint.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class CheckPoint : MonoBehaviour
 {
     [SerializeField] private float _deathYlevel;
+    [SerializeField] private float _offsetY; // Чтобы игрок спавнился выше,чем чекпоинт,когда это нужно
     //this method creates checkpoints
     // тег Player повесить на шарик, для работы чекпоинтов!!!
-    
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
         {
             if(other.gameObject.GetComponent<Player>().CurrentCheckPoint != transform.position)
             {
-                other.gameObject.GetComponent<Player>().CheckPoint(transform.position);
+                Vector3 checkPoint = transform.position;
+                checkPoint.y += _offsetY;
+                other.gameObject.GetComponent<Player>().CheckPoint(checkPoint);
                 other.gameObject.GetComponent<Player>().PlayerDeathLevelChecking(transform.position.y - _deathYlevel);
             }
         }
